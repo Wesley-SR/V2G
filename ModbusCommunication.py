8a1ab6b1717c59f38b3dc14381e709d212801525
Wesley-SR <wesley.sr@outlook.com>
2022-07-27 09:11:54 -0300

    Modbus Class

--- ModbusCommunication.py
+++ ModbusCommunication.py
@@ -8,19 +8,26 @@ class ModbusCommunication:
         # Load initial datas
         with open(config_file_path, 'r') as json_file:
             self.config_data = json.load(json_file)
-
-        # Init Modbus client
+        
+        # LOAD CONFIG PARAMETERS
+        self._host = self.config_data['host']
+        self._port = int(self.config_data['port'])
+        self._client_id = int(self.config_data['client_id'])
+        self._init_reg_read = int(self.config_data['init_reg_read'])
+        self._qtt_reg_read = int(self.config_data['qtt_reg_read'])
+        self._init_reg_write = int(self.config_data['init_reg_write'])
+        
+        # INIT MODBUS CLIENT
         try:
-            self._modbus_client = ModbusClient(host = self.config_data['host'],
-                            port=int(self.config_data['port']),
-                            unit_id = int(self.config_data['client_id']),
-                            debug=False, auto_open=True)
-        except ValueError:
-            print("Error with host or port params")
+            self._modbus_client = ModbusClient(host = self._host, port= self._port,
+                            unit_id = self._client_id, debug=False, auto_open=True)
+        except Exception as error:
+            print("Error creating Modbus client \n Error: {}".format(error))
     
-    def reed_modbus_data(self):
-        return self._modbus_client.read_holding_registers(int(self.config_data['init_reg_read']), int(self.config_data['qtt_reg_read']))
+    def read_modbus_data(self):
+        return self._modbus_client.read_holding_registers(self._init_reg_read, 
+                                                          self._qtt_reg_read)
     
     def write_modbus_data(self, data):
-        self._modbus_client.write_multiple_registers(int(self.config_data['init_reg_write']),data)
+        self._modbus_client.write_multiple_registers(self._init_reg_write, data)
         
