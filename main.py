1488d395986b2604f8451c5d8dbc9163474f5cd9
Wesley-SR <wesley.sr@outlook.com>
2022-07-27 11:19:11 -0300

    run a cycle

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # import ModbusCommunication
 from ModbusCommunication import ModbusCommunication
 from ModelPredictiveControl import ModelPredictiveControl
-from time import sleep
+# from time import sleep
 
 if __name__ == '__main__':
     
@@ -18,18 +18,13 @@ if __name__ == '__main__':
     
     except ValueError:
         print("Error: {}".format(ValueError))
-        
-    # measurement = Dados históricos medidos (Matriz de 144 linhas)
-    # forecast_use_bikes = matriz 144 posicoes
-    # forecast_pv_generation = matriz 14 posicoes
-    # optimization_results = resultado do algoritmo de otimização (Matriz de 144 linhas), vem a cada 10 minutos
-    # control_signal = Sinal de controle (vetor de 1 linha), é aplicado a cada 10 minutos
     
-    # while(continue_mpc):
+
+    while(continue_mpc):
         
         # READ MEASUREMENT DATAS
         modbus_measurement = modbus.read_modbus_data() # modbus_measurement = [PV P_R soc_est cx_bike1 ... cx_bike10 soc_bike1 ... soc_bike10] = Matrix 1x24
-        
+
         # RUN THE FORECAST ALGORITHMS
         mpc.set_new_data(modbus_measurement)
         
@@ -42,4 +37,5 @@ if __name__ == '__main__':
         # WRITE THE CONTROL SIGNALS
         modbus.write_modbus_data(control_signals)
         
-        # sleep(10*60)
+        continue_mpc = False
+        
